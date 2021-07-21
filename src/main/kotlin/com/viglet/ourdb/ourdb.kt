97b78af47c5af5497a02f77d012dcb6e9b4556de
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2021-07-20 23:10:10 -0300

    - Initial Commit

---
+++ src/main/kotlin/com/viglet/ourdb/ourdb.kt
@@ -0,0 +1,32 @@
+package com.viglet.ourdb
+
+import io.ktor.application.call
+import io.ktor.html.respondHtml
+import io.ktor.http.HttpStatusCode
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import kotlinx.html.*
+import com.viglet.ourdb.Version
+
+fun HTML.index() {
+    head {
+        title("Hello from Ktor!")
+    }
+    body {
+        div {
+            +"Hello from OurDB release ".plus(Version().getVersion());
+        }
+    }
+}
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "127.0.0.1") {
+        routing {
+            get("/") {
+                call.respondHtml(HttpStatusCode.OK, HTML::index)
+            }
+        }
+    }.start(wait = true)
+}
---
+++ src/main/kotlin/com/viglet/ourdb/version.kt
@@ -0,0 +1,7 @@
+package com.viglet.ourdb
+
+class Version {
+    public final fun getVersion(): String {
+        return "0.1";
+    }
+}
